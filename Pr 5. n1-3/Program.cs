b75c5fd29fb3c2898cf3ec4ec81abf342a9e81ae
hp <hp@HP>
2022-10-06 11:39:24 +0300

    Добавьте файлы проекта.

---
+++ Pr 5. n1-3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Math.E; //Присвоение переменной класса Math
+            Console.WriteLine("e = {0:F1}", x); //Число с плавающей точкой и одним знаков после запятой
+
+        }
+    }
+}
---
+++ n2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y; //Объявление переменных
+            do //Цикл
+            {
+                Console.Write("Введите высоту прямоугольник: "); //Вывод текста
+                x = double.Parse(Console.ReadLine()); //Присвоение переменной, которая считается с верхней строки
+                if (x <= 0) //Цикл
+                    Console.WriteLine("Введите положительно число"); //Вывод текста
+            } while (x <= 0);
+
+            do //Цикл
+            {
+                Console.Write("Введите длину прямоугольник: "); //Вывод текста
+                y = double.Parse(Console.ReadLine()); //Присвоение переменной, которая считается с верхней строки
+                if (y <= 0) //Цикл
+                    Console.WriteLine("Введите положительно число"); //Вывод текста
+            } while (y <= 0);
+
+            Console.WriteLine($"Периметр: {{0:F1}}", 2 * (x + y)); //Вывод указанных данных
+            Console.WriteLine($"Диагональ: {{0:F1}}", Math.Sqrt(x * x + y * y)); //Вывод указанных данных
+        }
+    }
+}
